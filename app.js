ead656a8f5821ea78eea1487ce3d3d34d65b5871
Sakpisit10 <114936196+kanban10@users.noreply.github.com>
2022-11-01 15:13:00 +0700

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var app = express();
 // mongoDB connect
 
 
-mongoose.connect('mongodb+srv://Ohm:faFy8ApPHTYArTdV@cluster0.6bbsnfu.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://Ohm:faFy8ApPHTYArTdV@cluster0.6bbsnfu.mongodb.net/url_history?retryWrites=true&w=majority');
 mongoose.Promise = global.Promise;
 
 
