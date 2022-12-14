dcd02a4b39a6bee79a49eadb807b4872e5b11557
Sakpisit <ohmandjoy@gmail.com>
2022-12-14 16:43:16 +0700

    update app.js

--- app.js
+++ app.js
@@ -34,7 +34,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', req.headers.origin);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers","*");
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     next();
 });
 
